322e4fe4e7adafc85de939c40188fb7ab5ddd684
rwaggoner <ryan.waggoner@keap.com>
2022-04-01 16:42:36 -0600

    Adding our first display message in the main to start the program.

--- src/com/ryan/prime/Main.java
+++ src/com/ryan/prime/Main.java
@@ -2,7 +2,39 @@ package com.ryan.prime;
 
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
-    }
+
+  public static void main(String[] args) {
+    startPrimeNumberGenerator();
+  }
+
+  private static void startPrimeNumberGenerator() {
+
+    //Create the first message the user will see when running the application
+    final String welcomeMessage = createStartMessage();
+
+    //Print out the instructions to our user
+    System.out.println(welcomeMessage);
+
+  }
+
+  /**
+   * Small helper method to create the welcoming / starting message of our program.
+   *
+   * @return A String with our message
+   */
+  private static String createStartMessage() {
+
+    //Create our buffer that we will use to build our string
+
+    //Small string messages with instructions
+    StringBuffer startMessageBuffer = new StringBuffer();
+    startMessageBuffer.append("Prime Number Coding Challenge");
+    startMessageBuffer.append("\n ********************************************** \n\n");
+    startMessageBuffer.append(
+        "This small program generator returns an ordered list of all prime numbers in a given range (inclusive of the endpoints).");
+
+    //Return our starting message as a string, to be re used pretty easily
+    return startMessageBuffer.toString();
+
+  }
 }
